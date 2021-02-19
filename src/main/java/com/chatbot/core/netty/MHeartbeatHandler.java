d0e95601bf07acc82f032ff9fc7cfcd10115f678
zhangxw_ <zhangxw_j@126.com>
2021-02-19 10:17:31 +0800

    完善当token错误,连接时重启的现象

--- src/main/java/com/chatbot/core/netty/MHeartbeatHandler.java
+++ src/main/java/com/chatbot/core/netty/MHeartbeatHandler.java
@@ -5,10 +5,12 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.EventLoop;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketHandshakeException;
 import io.netty.handler.timeout.IdleStateEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -19,10 +21,14 @@ public class MHeartbeatHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(MHeartbeatHandler.class);
 
+
+    @Autowired
+    private ConfigurableApplicationContext context;
     @Autowired
     private MClient client;
 
     int readIdleTimes = 0;
+    boolean token_error = false;
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
@@ -46,7 +52,6 @@ public class MHeartbeatHandler extends ChannelInboundHandlerAdapter {
             if (readIdleTimes > 5 ) {
                 // 6次未收到服务端的心跳回应了,关闭连接
                 ctx.channel().close();
-                log.info("服务端链接断开,1s后重新链接");
             }
         }else {
             super.userEventTriggered(ctx, evt);
@@ -55,7 +60,22 @@ public class MHeartbeatHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        // 断线直接开启重连
-        client.startClient();
+        if (token_error) {
+            // spring 关闭
+            context.close();
+            return;
+        }
+        // 断线开启重连
+        log.info("服务端链接断开,20s后重新链接");
+        ctx.channel().eventLoop().schedule(() ->client.startClient(),20L, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        if (cause instanceof WebSocketHandshakeException) {
+            log.error("链接异常,请检查token!");
+            token_error = true;
+        }
+        ctx.channel().close();
     }
 }
